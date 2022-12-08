4c122bc22ea775d0be657be3002e17cc1b2d1135
Barbara Peres <barbarabelyaeva13@gmail.com>
2022-12-09 00:59:52 +0300

    added tasks seminar 2

---
+++ task12/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напиши программу, которая будет принимать на вход два числа и выводить, является ли первое число кратным второму. 
+// Если число 1 не кратно числу 2, то программа выводит остаток от деления.
+// Например:
+// 34, 5 -> не кратно, остаток 4
+// 16, 4 -> кратно
+
+Console.WriteLine("Введи первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введи второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+int result = number1 % number2;
+if (result == 0)
+{
+    Console.WriteLine("Кратно");
+}
+else
+{
+    Console.WriteLine("Не кратно, остаток " + result);
+}
---
+++ task14/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23
+// Например:
+// 14 -> нет
+// 46 -> нет
+// 161 -> да
+
+Console.WriteLine("Введи число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int krat1 = 7;
+int krat2 = 23;
+
+if ((number % krat1 == 0)&&(number % krat2 == 0))
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
