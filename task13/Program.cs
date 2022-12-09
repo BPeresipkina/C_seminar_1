bedf48efc557edbb9309e790d35507aa4008d2e7
Barbara Peres <barbarabelyaeva13@gmail.com>
2022-12-09 12:58:09 +0300

    added homework

--- task13/Program.cs
+++ task13/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int div = 10;
+int result = 0;
+int i = 1;
+
+while (number / div > 0)
+{
+    div = div * 10;
+    i++;
+}
+
+// Console.WriteLine(i);
+
+if (i > 2)
+ {
+     i = i - 3;
+     div = 10;
+
+     while (i > 0)
+     {
+         div = div * 10;
+         i = i - 1;
+     }
+
+    //  Console.WriteLine(div);
+
+     result = number % div / (div / 10);
+
+     Console.WriteLine(result);
+ }
+ else
+ {
+     Console.WriteLine("Третьей цифры нет");
+ }
--- task15/Program.cs
+++ task15/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.WriteLine("Введи номер дня недели: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 5 && number < 8)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
